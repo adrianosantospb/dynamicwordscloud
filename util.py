1bb42f566b870b960d88b969cbdf5672377185cc
Joaquim Honório <joaquim.cmh@gmail.com>
2018-02-23 07:07:26 -0300

    Comunicação finalizada entre as novas telas

--- util.py
+++ util.py
@@ -4,8 +4,7 @@ import json
 filename = "words.json"
 
 #read json
-def read_json(filename):
-    print ("read json")
+def read_json():
     if not default_json_exists(filename):
         create_default_json(filename)
         print ("new json file created")
@@ -15,7 +14,7 @@ def read_json(filename):
     return data
 
 #write json data
-def write_json(data, filename):
+def write_json(data):
     jsonFile = open(filename, 'w+')
     jsonFile.write(json.dumps(data))
     jsonFile.close()
@@ -31,8 +30,29 @@ def default_json_exists (filename):
         return False;
     return True;
 
+# return index
+def word_exists (data, word):
+    for i in range (len(data["words"])):
+        if data["words"][i]["word"] == word:
+            return i
+    return -1
+
+def add_word (words):
+    d = read_json()
+    for pos in range(len(words)):
+        wrd = words[pos]
+        indx = word_exists(d, wrd)
+        if indx > -1:
+            d["words"][indx]["repetitions"] = int(d["words"][indx]["repetitions"]) + 1
+        else:
+            d["words"].append({"word": wrd, "repetitions": 1})
+    write_json(d)
 
 
-
-
+def save_updates (json_data):
+    final = '{"words" :' + json_data + '}'
+    j = json.loads(final)
+    for pos in range(len(j["words"])):
+        j["words"][pos]["repetitions"]=int(j["words"][pos]["repetitions"])
+    write_json(j)
 
